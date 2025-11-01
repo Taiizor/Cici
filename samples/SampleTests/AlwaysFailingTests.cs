44f01bb48eedafadc5e6908b0903df9d08a1d2db
Taiizor <41683699+Taiizor@users.noreply.github.com>
2025-11-01 15:48:27 +0300

    Refactor test class namespace declarations
    Updated AlwaysFailingTests, FlakyTests, and StableTests to use block-style namespace declarations for consistency. No functional changes to test logic.

--- samples/SampleTests/AlwaysFailingTests.cs
+++ samples/SampleTests/AlwaysFailingTests.cs
@@ -1,47 +1,48 @@
 using Xunit;
 
-namespace SampleTests;
-
-/// <summary>
-/// These tests always fail - representing broken tests that need fixing
-/// </summary>
-public class AlwaysFailingTests
+namespace SampleTests
 {
-    [Fact]
-    public void BrokenAssertion_AlwaysFails()
+    /// <summary>
+    /// These tests always fail - representing broken tests that need fixing
+    /// </summary>
+    public class AlwaysFailingTests
     {
-        Assert.Equal(5, 2 + 2); // This will always fail
-    }
+        [Fact]
+        public void BrokenAssertion_AlwaysFails()
+        {
+            Assert.Equal(5, 2 + 2); // This will always fail
+        }
     
-    [Fact]
-    public void NullReferenceTest_AlwaysFails()
-    {
-        string? nullString = null;
-        Assert.NotNull(nullString); // This will always fail
-        var length = nullString!.Length; // Would throw if assertion didn't fail first
-    }
+        [Fact]
+        public void NullReferenceTest_AlwaysFails()
+        {
+            string? nullString = null;
+            Assert.NotNull(nullString); // This will always fail
+            var length = nullString!.Length; // Would throw if assertion didn't fail first
+        }
     
-    [Fact]
-    public void ExceptionTest_AlwaysFails()
-    {
-        throw new InvalidOperationException("This test always throws an exception");
-    }
+        [Fact]
+        public void ExceptionTest_AlwaysFails()
+        {
+            throw new InvalidOperationException("This test always throws an exception");
+        }
     
-    [Fact]
-    public async Task AsyncTimeout_AlwaysFails()
-    {
-        // Simulates a test that always times out
-        await Task.Delay(60000); // 60 second delay (will timeout)
-        Assert.Fail("This should never be reached");
-    }
+        [Fact]
+        public async Task AsyncTimeout_AlwaysFails()
+        {
+            // Simulates a test that always times out
+            await Task.Delay(3000); // 3 second delay (will timeout)
+            Assert.Fail("This should never be reached");
+        }
     
-    [Theory]
-    [InlineData(1, 2, 0)] // Wrong expected value
-    [InlineData(2, 3, 0)] // Wrong expected value
-    [InlineData(5, 5, 0)] // Wrong expected value
-    public void WrongExpectations_AlwaysFails(int a, int b, int expected)
-    {
-        var actual = a + b;
-        Assert.Equal(expected, actual); // Will always fail due to wrong expected values
+        [Theory]
+        [InlineData(1, 2, 0)] // Wrong expected value
+        [InlineData(2, 3, 0)] // Wrong expected value
+        [InlineData(5, 5, 0)] // Wrong expected value
+        public void WrongExpectations_AlwaysFails(int a, int b, int expected)
+        {
+            var actual = a + b;
+            Assert.Equal(expected, actual); // Will always fail due to wrong expected values
+        }
     }
 }
--- samples/SampleTests/StableTests.cs
+++ samples/SampleTests/StableTests.cs
@@ -1,50 +1,51 @@
 using Xunit;
 
-namespace SampleTests;
-
-/// <summary>
-/// These tests should always pass consistently
-/// </summary>
-public class StableTests
+namespace SampleTests
 {
-    [Fact]
-    public void MathAddition_ShouldAlwaysPass()
+    /// <summary>
+    /// These tests should always pass consistently
+    /// </summary>
+    public class StableTests
     {
-        var result = 2 + 2;
-        Assert.Equal(4, result);
-    }
+        [Fact]
+        public void MathAddition_ShouldAlwaysPass()
+        {
+            var result = 2 + 2;
+            Assert.Equal(4, result);
+        }
     
-    [Fact]
-    public void StringConcatenation_ShouldAlwaysPass()
-    {
-        var result = "Hello" + " " + "World";
-        Assert.Equal("Hello World", result);
-    }
+        [Fact]
+        public void StringConcatenation_ShouldAlwaysPass()
+        {
+            var result = "Hello" + " " + "World";
+            Assert.Equal("Hello World", result);
+        }
     
-    [Fact]
-    public void ListOperations_ShouldAlwaysPass()
-    {
-        var list = new List<int> { 1, 2, 3 };
-        list.Add(4);
-        Assert.Equal(4, list.Count);
-        Assert.Contains(4, list);
-    }
+        [Fact]
+        public void ListOperations_ShouldAlwaysPass()
+        {
+            var list = new List<int> { 1, 2, 3 };
+            list.Add(4);
+            Assert.Equal(4, list.Count);
+            Assert.Contains(4, list);
+        }
     
-    [Fact]
-    public async Task AsyncOperation_ShouldAlwaysPass()
-    {
-        await Task.Delay(10); // Short, consistent delay
-        var result = await Task.FromResult(42);
-        Assert.Equal(42, result);
-    }
+        [Fact]
+        public async Task AsyncOperation_ShouldAlwaysPass()
+        {
+            await Task.Delay(10); // Short, consistent delay
+            var result = await Task.FromResult(42);
+            Assert.Equal(42, result);
+        }
     
-    [Theory]
-    [InlineData(1, 1, 2)]
-    [InlineData(2, 3, 5)]
-    [InlineData(10, 15, 25)]
-    public void ParameterizedTest_ShouldAlwaysPass(int a, int b, int expected)
-    {
-        var result = a + b;
-        Assert.Equal(expected, result);
+        [Theory]
+        [InlineData(1, 1, 2)]
+        [InlineData(2, 3, 5)]
+        [InlineData(10, 15, 25)]
+        public void ParameterizedTest_ShouldAlwaysPass(int a, int b, int expected)
+        {
+            var result = a + b;
+            Assert.Equal(expected, result);
+        }
     }
 }
